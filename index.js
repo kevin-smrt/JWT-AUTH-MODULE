20759cdc6b4c12df9ccdd42a193da7d2d114a715
kevin_smrt <kevinsanmartin@live.fr>
2021-09-06 12:14:54 +0200

    init server with route /posts

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+const posts = [
+    {
+        username: "Bob",
+        title: "Post 1"
+    },
+    {
+        usernae: "Billy",
+        title: "Post 2"
+    }
+]
+
+router.get('/posts', (req, res) => {
+    res.json(posts);
+});
+
+app.listen(3000);
