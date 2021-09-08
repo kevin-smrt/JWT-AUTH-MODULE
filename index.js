e3f7cbfc509d6b1cb507c49d3ef59461193fcd3f
kevin_smrt <kevinsanmartin@live.fr>
2021-09-08 18:38:43 +0200

    add dotenv

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
 const router = require('./router');
 
 const app = express();
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
 app.use(router);
 
-app.listen(3000);
+app.listen(PORT, () => {
+    console.log(`Server ready on http://localhost/${PORT}`);
+});
